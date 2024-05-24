a70f8dd1bb1684c47dc8da2fb880b3f3293505c6
Thiago Metzner <thiago.metzner@sisplansistemas.com.br>
2024-05-24 06:19:48 -0300

    feat: init side bar

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, User } from "lucide-react";
 import Link from "next/link";
 import { Card, CardContent } from "./ui/card";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 const Header = () => {
   return (
@@ -16,7 +18,41 @@ const Header = () => {
             </Button>
           </SheetTrigger>
 
-          <SheetContent side="left" className="p-0">{/* <SideMenu /> */}</SheetContent>
+          <SheetContent side="left" className="p-0">{/* <SideMenu /> */}
+            <SheetHeader className="flex">
+              <SheetTitle className="flex justify-center p-5">Menu</SheetTitle>
+              <SheetDescription className="flex justify-center">
+                <div>
+                  <h1>Thiago</h1>
+                  <h2>Empresa</h2>
+                </div>
+              </SheetDescription>
+            </SheetHeader>
+
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <ul className="w-full">
+                  <li className="flex py-2">
+                    <User />
+                    <Link href='/usuario'>
+                      <Label htmlFor="name" className="text-right">Usuário</Label>
+                    </Link>
+                  </li>
+                  <li className="flex py-2">
+                    <User />
+                    <Link href='/usuario'>
+                      <Label htmlFor="name" className="text-right">Usuário</Label>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit">Save changes</Button>
+              </SheetClose>
+            </SheetFooter> */}
+          </SheetContent>
         </Sheet>
         {/* <Image
           src="/Logo.png"
